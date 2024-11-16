7b7bca0a0e45d8cbe298a1d965693793b873bd8d
leouev <leouev@gmail.com>
2024-11-15 21:39:04 -0500

    update test cases

--- 1TwoSum/test.py
+++ 1TwoSum/test.py
@@ -1,6 +1,16 @@
 import twoSum
 
 s1 = twoSum.Solution()
-num1 = [3, 1, 2, 4]
-target1 = 6
-print(s1.twoSum(num1,target1))      
+num1 = [2,7,11,15]
+target1 = 9
+print(s1.twoSum(num1,target1))   
+
+s2 = twoSum.Solution()
+num2 = [3,2,4]
+target2 = 6
+print(s2.twoSum(num2,target2))
+
+s3 = twoSum.Solution()
+num3 = [3,3]
+target3 = 6
+print(s2.twoSum(num3,target3))
